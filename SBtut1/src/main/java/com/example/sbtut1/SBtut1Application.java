511703a40df4318cfe9421e8da2166ee80ff9bd2
Bhavishya Tiwari <bhavishyatiwari2000@gmail.com>
2024-08-25 19:51:16 +0530

    First commit

---
+++ SBtut1/src/main/java/com/example/sbtut1/SBtut1Application.java
@@ -0,0 +1,13 @@
+package com.example.sbtut1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBtut1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SBtut1Application.class, args);
+    }
+
+}
---
+++ SBtut1/src/test/java/com/example/sbtut1/SBtut1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbtut1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBtut1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
